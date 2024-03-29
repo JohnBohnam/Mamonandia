4cf8b0fbb3e459bda1a9ee83fd42d90da3a5f97e
adams3141 <Adam.strach@student.uj.edu.pl>
2024-03-29 12:08:21 +0100

    Adam testuje 2

--- Adam_tests.py
+++ Adam_tests.py
@@ -2,16 +2,18 @@ from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 import string
 
+
 # - `STARFRUIT`: 20
 # AMETHYSTS
 
 class Trader:
-  
+
     def __init__(self):
         self.products = ["STARFRUIT", "AMETHYSTS"]
         for product in self.products:
             print(f"{product} lowest ask price; {product} highest bid price;", end="")
-        print("obs bidPrice; obs askPrice; obs transportFees; obs exportTariff; obs importTariff; obs sunlight; obs humidity\n")
+        print(
+            "obs bidPrice; obs askPrice; obs transportFees; obs exportTariff; obs importTariff; obs sunlight; obs humidity\n")
         # self.huj = 0
 
     def run(self, state: TradingState):
@@ -19,34 +21,35 @@ class Trader:
         for product in self.products:
             orders: List[Order] = []
             order_depth: OrderDepth = state.order_depths[product]
-            if(product in state.order_depths):
-                lowestAskPrice = state.order_depths[product].keys().sort()[0]
-                highestBidPrice = state.order_depths[product].keys().sort()[-1]
+            if (product in state.order_depths):
+                temp = list(order_depth.sell_orders.keys())
+                temp.sort()
+                temp_buy = list(order_depth.buy_orders.keys())
+                temp_buy.sort()
+                lowestAskPrice = temp[0]  # state.order_depths[product].keys().sort()[0]
+                highestBidPrice = temp_buy[-1]
                 print(f"{lowestAskPrice}; {highestBidPrice};", end="")
 
-
-                mean_price=(lowestAskPrice+highestBidPrice)/2
+                mean_price = (lowestAskPrice + highestBidPrice) / 2
                 if len(order_depth.sell_orders) != 0:
-                    best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]#posortowane
-                if int(best_ask) < mean_price*0.95:
-                    print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
-    
+                    best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]  # posortowane
+                    if int(best_ask) < mean_price * 0.95:
+                        print("BUY", str(-best_ask_amount) + "x", best_ask)
+                        orders.append(Order(product, best_ask, -best_ask_amount))
+
                 if len(order_depth.buy_orders) != 0:
                     best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > mean_price*1.05:
-                    print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
-            
+                    if int(best_bid) > mean_price * 1.05:
+                        print("SELL", str(best_bid_amount) + "x", best_bid)
+                        orders.append(Order(product, best_bid, -best_bid_amount))
+
                 result[product] = orders
 
-           # else:
-            #    print("-1; -1;", end="")
-        
-            
-    
-		    # String value holding Trader state data required. 
-				# It will be delivered as TradingState.traderData on next execution.
-        traderData = "SAMPLE" 
-        
+        # else:
+        #    print("-1; -1;", end="")
+
+        # String value holding Trader state data required.
+        # It will be delivered as TradingState.traderData on next execution.
+        traderData = "SAMPLE"
+
         return result, 0, traderData
