896d6f0c8c3cdbea35ef708bc688ce57b0ad5352
adams3141 <Adam.strach@student.uj.edu.pl>
2024-03-30 19:40:37 +0100

    amethyst-final trader and commented

--- Adam_tests.py
+++ Adam_tests.py
@@ -34,32 +34,30 @@ class Trader:
                     self.limit_hits_down[product] += 1
 
                 if product == "AMETHYSTS":
-                    amethyst_price = 10000
-                    amethyst_buy = 10001
-                    amethyst_sell = 9999
+                    amethyst_price = 10000#fair price
+                    amethyst_buy = 9999#max price that we want to pay to buy
+                    amethyst_sell = 10001#minimal price that we want to sell
 
                     if len(order_depth.sell_orders) != 0:
-                        best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]  # posortowane
-                        if int(best_ask) < amethyst_price:
-                            # print("BUY", str(-best_ask_amount) + "x", best_ask)
-                            if pos > -20:
-                                buy_amount = 20 - pos
-                                orders.append(Order(product, amethyst_buy, buy_amount))
-                       # elif pos < 0:
-                       #     orders.append(Order(product, amethyst_price, -pos))
+                        best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]  # sorted
+                        if int(best_ask) < amethyst_price:#if best ask price is lower than fair we want to buy
+                            if pos < 20:#check if we are full
+                                buy_amount = 20 - pos#max that we can buy
+                                orders.append(Order(product, amethyst_buy, buy_amount))#buy order
+                        elif pos < 0:#if we are short on amethyst we want to buy at fair price
+                            orders.append(Order(product, amethyst_price, -pos))#buy
 
                     if len(order_depth.buy_orders) != 0:
                         best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                        if int(best_bid) > amethyst_price:
-                            # print("SELL", str(best_bid_amount) + "x", best_bid)
-                            if pos < 20:
-                                sell_amount = 20 + pos
-                                orders.append(Order(product, amethyst_sell, -sell_amount))
-                        #elif pos > 0:
-                        #    orders.append(Order(product, amethyst_price, -pos))
+                        if int(best_bid) > amethyst_price:#if best bid price is higer than fair we want to sell
+                            if pos > -20:#check if we are full
+                                sell_amount = -20-pos
+                                orders.append(Order(product, amethyst_sell, sell_amount))#sell
+                        elif pos > 0:#if we have amethyst we want to sell at fair price
+                            orders.append(Order(product, amethyst_price, -pos))#sell
 
                 result[product] = orders
-
+        #hits stock limit
         traderData = "SAMPLE"
         print(f"limits up: {self.limit_hits_up}, limits down: {self.limit_hits_down}\n")
         print(f"runs: {self.runs}\n")
