3ebfc09a278ed6ade9530c6723e01e1621f3bdd9
JohnBohnam <fanofledzeppelin@gmail.com>
2024-04-08 15:21:42 +0200

    see the profit bug

--- backtest/backtester.py
+++ backtest/backtester.py
@@ -18,12 +18,12 @@ import time
 
 # Adjust accordingly the round and day to your needs
 if __name__ == "__main__":
-    curr_trader = Trader()
+    curr_trader = Trader(verbose=True)
     max_time = 9  # int(input("Max timestamp (1-9)->(1-9)(00_000) or exact number): ") or 999000)
     if max_time < 10:
         max_time *= 100000
     round_ = 1  # int(input("Input a round (blank for 4): ") or 4)
-    day = -2  # int(input("Input a day (blank for random): ") or random.randint(1, 3))
+    # day = -2  # int(input("Input a day (blank for random): ") or random.randint(1, 3))
     names_in = "n"  # input("With bot names (default: y) (y/n): ")
     names = True
     if 'n' in names_in:
@@ -32,8 +32,9 @@ if __name__ == "__main__":
     halfway = False
     if 'y' in halfway_in:
         halfway = True
+
+    # for day in range(-2, 1):
+    day = -2
     print(f"Running simulation on round {round_} day {day} for time {max_time}")
-
-    profits = simulate_alternative(round_, day, curr_trader, max_time, names, halfway=halfway, plotting=True)
-
+    profits = simulate_alternative(round_, day, curr_trader, max_time, names, halfway=halfway, verbose=False, plotting=False, logging=False)
     print(f"profits: {profits}")
