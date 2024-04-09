e5d4a38a6b499fdc3896d1bf0b14e3fc075bcf06
JohnBohnam <fanofledzeppelin@gmail.com>
2024-04-10 00:36:02 +0200

    fit price

--- backtest/vladbot1.py
+++ backtest/vladbot1.py
@@ -11,35 +11,6 @@ def get_mid_price(od: OrderDepth):
     return (min(od.buy_orders.keys()) + max(od.sell_orders.keys())) // 2
 
 
-def get_trader_volume(buy_orders: Dict[int, int], sell_orders: Dict[int, int], price):
-    buy_volume = 0
-    for bid_price, volume in buy_orders.items():
-        if bid_price >= price:
-            buy_volume += volume
-    sell_volume = 0
-    for ask_price, volume in sell_orders.items():
-        if ask_price <= price:
-            sell_volume += volume
-    return min(buy_volume, sell_volume)
-
-
-def fit_price(buy_orders: Dict[int, int], sell_orders: Dict[int, int]):
-    if not buy_orders or not sell_orders:
-        return None
-
-    min_bid = min(buy_orders.keys())
-    max_ask = max(sell_orders.keys())
-
-    best_fit_price = None
-    best_fit = 0
-    for price in range(min_bid, max_ask):
-        curr_fit = get_trader_volume(buy_orders, sell_orders, price)
-        if curr_fit > best_fit:
-            best_fit = curr_fit
-            best_fit_price = price
-    return best_fit_price
-
-
 class Trader:
 
     def __init__(self, sell_margin=1, buy_margin=1, time_window=2, verbose=False):
