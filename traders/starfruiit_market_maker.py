b55b17d2d6beea6419c75da779f39035afed07e1
Vlad <snozyk.v@gmail.com>
2024-04-11 01:13:05 +0200

    stairfruit market maker

--- traders/starfruiit_market_maker.py
+++ traders/starfruiit_market_maker.py
@@ -1,5 +1,5 @@
 from typing import List, Dict
-
+import numpy as np
 from datamodel import OrderDepth, TradingState, Order, Time
 
 class Trader:
@@ -7,6 +7,7 @@ class Trader:
 	def __init__(self, verbose=False):
 		self.spread = 5
 		self.verbose = verbose
+		self.single_position_limit = 7
 		
 	
 
@@ -21,17 +22,18 @@ class Trader:
 		best_ask = min(state.order_depths["STARFRUIT"].sell_orders.keys()) if state.order_depths["STARFRUIT"].sell_orders else 0
 		spread = best_ask - best_bid
 		if spread>self.spread:
-			mid = (best_ask + best_bid) / 2
-			sell_for = int(mid + self.spread/2)
-			result["STARFRUIT"].append(Order("STARFRUIT", sell_for, -20 - pos))
-			if self.verbose:
-				print("time", state.timestamp)
-				print(f"pos: {pos}, selling {-20 - pos} of STARFRUIT for {sell_for}")
+			mid = (best_ask + best_bid) / 2 - pos/10
+			sell_for = int(np.round(mid + self.spread/2))
+			q = -self.single_position_limit
+			q = max(q, -20 - pos)
+			result["STARFRUIT"].append(Order("STARFRUIT", sell_for, q))
 
-			buy_for = int(mid - self.spread/2)
-			result["STARFRUIT"].append(Order("STARFRUIT", buy_for, 20 - pos))
-			if self.verbose:
-				print("time", state.timestamp)
-				print(f"pos: {pos}, buying {20 - pos} of STARFRUIT for {buy_for}")
+
+			buy_for = int(round(mid - self.spread/2))
+			q = self.single_position_limit
+			q = min(q, 20 - pos)
+			result["STARFRUIT"].append(Order("STARFRUIT", buy_for, q))
+			print(f"time: {state.timestamp}, pos: {pos}")
+
 
 		return result, 0, ""
