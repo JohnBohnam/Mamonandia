4d9951de0652202c1ea21b0499b676e5b92572ff
JohnBohnam <fanofledzeppelin@gmail.com>
2024-04-11 16:13:40 +0200

    algo cleanup

--- traders/sumbission.py
+++ traders/sumbission.py
@@ -19,7 +19,6 @@ class Trader:
         for key, value in params.items():
             setattr(self, key, value)
 
-    # good params so far: buy_margin=5, sell_margin=5, time_window=2, min_trend=0.000281, trend_sensitivity=1
     def __init__(self, buy_margin=4, sell_margin=4, time_window=2, verbose=False):
         self.limit_hits_up = {}
         self.limit_hits_down = {}
@@ -35,23 +34,14 @@ class Trader:
         self.spread = 5
         self.single_position_limit = 5
 
-
-        # print(f"buy_margin: {buy_margin}, sell_margin: {sell_margin}, time_window: {time_window}")
-
-        # the bigger the time window, the more conservative the trader
         self.time_window = time_window
 
         self.target_price = 10000
 
-
-        # the lesser the margin, the more aggressive the trader
         self.buy_margin = buy_margin
         self.sell_margin = sell_margin
 
         self.verbose = verbose
-
-        # these are the stupid prams that make stupid profits
-
         self.products = ["STARFRUIT", "AMETHYSTS"]
 
         for product in self.products:
@@ -97,38 +87,34 @@ class Trader:
 
         pos = state.position[product] if product in state.position else 0
 
-        # od = state.order_depths[product]
         sell_margin = self.sell_margin
         buy_margin = self.buy_margin
 
         sell_q = 0
         buy_q = 0
 
+        lim = self.limits[product]
+
+        ods = state.order_depths[product]
 
         if self.prev_bids:
             sell_for = int(np.min(self.prev_bids) + sell_margin)
-            # sell_q = -20-pos
-            sell_q = calculate_sell_quantity(state.order_depths["STARFRUIT"], sell_for)
-            sell_q = max(sell_q, -20 - pos)
+            sell_q = calculate_sell_quantity(ods, sell_for)
+            sell_q = max(sell_q, -lim - pos)
             orders.append(Order(product, sell_for, sell_q))
             if self.verbose:
                 print(f"pos: {pos}, selling {sell_q} of {product} for {sell_for}")
 
         if self.prev_asks:
             buy_for = int(np.max(self.prev_asks) - buy_margin)
-            # buy_q = 20 - pos
-            buy_q = calculate_buy_quantity(state.order_depths["STARFRUIT"], buy_for)
-            buy_q = min(buy_q, 20 - pos)
+            buy_q = calculate_buy_quantity(ods, buy_for)
+            buy_q = min(buy_q, lim - pos)
             orders.append(Order(product, buy_for, buy_q))
             if self.verbose:
                 print(f"pos: {pos}, buying {buy_q} of {product} for {buy_for}")
-        # print(orders)
 
-        # pos = state.position["STARFRUIT"] if "STARFRUIT" in state.position else 0
-        best_bid = max(state.order_depths["STARFRUIT"].buy_orders.keys()) if state.order_depths[
-            "STARFRUIT"].buy_orders else 0
-        best_ask = min(state.order_depths["STARFRUIT"].sell_orders.keys()) if state.order_depths[
-            "STARFRUIT"].sell_orders else 0
+        best_bid = max(ods.buy_orders.keys()) if ods.buy_orders else 0
+        best_ask = min(ods.sell_orders.keys()) if ods.sell_orders else 0
         spread = best_ask - best_bid
         if spread > self.spread:
             mid = (best_ask + best_bid) / 2 - pos / 10
@@ -137,67 +123,16 @@ class Trader:
             q = max(q, -20 - pos - sell_q)
             q = min(q, 0)
             if q != 0:
-                orders.append(Order("STARFRUIT", sell_for, q))
+                orders.append(Order(product, sell_for, q))
 
             buy_for = int(round(mid - self.spread / 2))
             q = self.single_position_limit
             q = min(q, 20 - pos - buy_q)
             q = max(q, 0)
             if q != 0:
-                orders.append(Order("STARFRUIT", buy_for, q))
+                orders.append(Order(product, buy_for, q))
 
         return orders
-    #
-    # def order_amethysts(self, state: TradingState) -> list[Order]:
-    #     orders = []
-    #     product = "AMETHYSTS"
-    #
-    #     order_depth = state.order_depths[product]
-    #     pos = state.position.get(product, 0)
-    #     limit_buy, limit_sell = 20 - pos, -20 - pos
-    #     # buy cheap items
-    #
-    #     q = calculate_buy_quantity(order_depth, self.target_price - 1)
-    #     q = min(q, limit_buy)
-    #     if q != 0:
-    #         limit_buy -= q
-    #         pos += q
-    #         orders.append(Order(product, self.target_price - 1, q))
-    #     #
-    #     # sell expensive items
-    #     q = calculate_sell_quantity(order_depth, self.target_price + 1)
-    #     q = max(q, -20 - pos)
-    #     if q != 0:
-    #         limit_sell -= q
-    #         pos += q
-    #         orders.append(Order(product, self.target_price + 1, q))
-    #     #
-    #
-    #     # make 0 position
-    #     if pos > 0:
-    #         q = calculate_sell_quantity(order_depth, self.target_price)
-    #         q = max(q, limit_sell)
-    #         if q != 0:
-    #             pos += q
-    #             limit_sell -= q
-    #             orders.append(Order(product, self.target_price, q))
-    #     elif pos < 0:
-    #         q = calculate_buy_quantity(order_depth, self.target_price)
-    #         q = min(q, limit_buy)
-    #         if q != 0:
-    #             pos += q
-    #             limit_buy -= q
-    #             orders.append(Order(product, self.target_price, q))
-    #
-    #     # send MM orders
-    #     buy_q = limit_buy
-    #     sell_q = limit_sell
-    #     if buy_q > 0:
-    #         orders.append(Order(product, self.target_price - 1 + buy_q // 40, buy_q))
-    #     if sell_q < 0:
-    #         orders.append(Order(product, self.target_price + 1 + sell_q // 40 + 1, sell_q))
-    #
-    #     return orders
 
     def order_amethysts(self, state: TradingState) -> list[Order]:
         orders = []
@@ -205,7 +140,9 @@ class Trader:
 
         order_depth = state.order_depths[product]
         pos = state.position.get(product, 0)
-        limit_buy, limit_sell = 20 - pos, -20 - pos
+
+        lim = self.limits[product]
+        limit_buy, limit_sell = lim - pos, -lim - pos
 
         q = calculate_buy_quantity(order_depth, self.target_price - self.half_spread)
         q = min(q, limit_buy)
@@ -215,7 +152,7 @@ class Trader:
             orders.append(Order(product, self.target_price - self.half_spread, q))
 
         q = calculate_sell_quantity(order_depth, self.target_price + self.half_spread)
-        q = max(q, -20 - pos)
+        q = max(q, -lim - pos)
         if q != 0:
             limit_sell -= q
             pos += q
@@ -249,13 +186,11 @@ class Trader:
         print(f"buy_margin: {self.buy_margin}, sell_margin: {self.sell_margin}, single_position_limit: {self.single_position_limit}")
         print(f"time_window: {self.time_window}")
         self.runs += 1
-        # print(f"runs: {self.runs}\n")
         self.update_limit_hits(state)
 
         print(f"limit_hits_up: {self.limit_hits_up}, limit_hits_down: {self.limit_hits_down}")
 
         result = {"STARFRUIT": self.order_starfruit(state), "AMETHYSTS": self.order_amethysts(state)}
-        # result = {"STARFRUIT": [], "AMETHYSTS": self.order_amethysts(state)}
 
         self.update_prevs("STARFRUIT", state)
 
